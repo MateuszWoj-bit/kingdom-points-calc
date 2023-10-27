c0cb4679939371bcfbbed3958ad352f90dd6211f
Mateusz Wójcikowski <yankes90@gmail.com>
2023-10-27 05:05:28 +0200

    features

--- index.js
+++ index.js
@@ -1,38 +1,75 @@
 function calculateTimeToAchievePoints() {
-  var pointsInput = document.getElementById("points"); // Assume you have an input field with the id "points"
-  var dailyPointsInput = document.getElementById("dailyPoints"); // Assume you have an input field with the id "dailyPoints"
-  var modeSelect = document.getElementById("mode"); // Assume you have a mode selection dropdown with the id "mode"
+  var pointsInput = document.getElementById("points");
+  var dailyPointsInput = document.getElementById("dailyPoints");
+  var modeSelect = document.getElementById("mode");
+  var daysPlayedInput = document.getElementById("daysPlayed");
+  var resultDiv = document.getElementById("result"); // Reference to the result div
 
   var points = parseFloat(pointsInput.value);
   var dailyPoints = parseFloat(dailyPointsInput.value);
-var mode = modeSelect.value;
-    
-    if (points < dailyPoints) {
-    return alert("wtf")
-}
+  var mode = modeSelect.value;
+  var daysPlayed = parseFloat(daysPlayedInput.value);
 
-  points -= Math.min(dailyPoints, 600);
-
-  if (mode === "mode1") {
-    var roundsNeeded = points / 3; // Assuming an average of 3 points per round
-    var roundsText = "Total rounds to play: " + roundsNeeded;
-  } else if (mode === "mode2") {
-    var hoursNeeded = points / 150; // Assuming 150 points per hour
-    var roundsText = "Total hours needed in mode 2: " + hoursNeeded;
+  if (points < daysPlayed * dailyPoints) {
+    resultDiv.innerHTML = "Daily Kingdom points are enough";
+    return;
   }
 
-  var minutesNeeded = roundsNeeded * 3; // Assuming an average of 3 minutes per round
-  var hoursNeeded = minutesNeeded / 60;
-  var daysNeeded = hoursNeeded / 24;
+  points -= daysPlayed * dailyPoints; // Deduct points for each day played
 
-  alert(
-    "Time needed to achieve " +
+  if (mode === "mode1") {
+    var roundsNeeded = Math.ceil(points / 3); // Assuming an average of 3 points per round
+  } else if (mode === "mode2") {
+    var hoursNeeded = points / 150; // Assuming 150 points per hour
+  }
+
+  var durationMinutes =
+    mode === "mode1" ? Math.ceil(roundsNeeded * 1.17) : hoursNeeded * 60;
+  var endDate = new Date();
+  endDate.setMinutes(endDate.getMinutes() + durationMinutes);
+
+  var daysNeeded = Math.floor(durationMinutes / (60 * 24));
+  var hoursLeft = Math.floor((durationMinutes % (60 * 24)) / 60);
+  var minutesLeft = durationMinutes % 60;
+
+  var options = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+  var endDateString = endDate.toLocaleDateString("en-US", options);
+
+  if (mode === "mode1") {
+    resultDiv.innerHTML =
+      "Points to achieve (after subtracting daily Kingdom points): " +
       points +
-      " points (after subtracting daily points): approximately " +
+      "<br>" +
+      "Time needed to achieve necessary points: approximately " +
       daysNeeded +
-      " days and " +
-      (hoursNeeded % 24) +
-      " hours.\n" +
-      roundsText
-  );
+      " days, " +
+      hoursLeft +
+      " hours, and " +
+      minutesLeft +
+      " minutes." +
+      "<br>" +
+      "Rounds needed: " +
+          roundsNeeded;
+      resultDiv.style.display = "block";
+  } else {
+    resultDiv.innerHTML =
+      "Points to achieve (after subtracting daily Kingdom points): " +
+      points +
+      "<br>" +
+      "Time needed to achieve necessary points: approximately " +
+      daysNeeded +
+      " days, " +
+      hoursLeft +
+      " hours, and " +
+      minutesLeft +
+      " minutes." +
+          "<br>";
+      resultDiv.style.display = "block";
+  }
 }
